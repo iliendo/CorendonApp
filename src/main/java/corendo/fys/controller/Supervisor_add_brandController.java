08cd84cb7ece2974bdb3701a352459a5d0620aeb
Ilias Azagagh <ilias.azagagh@gmail.com>
2017-12-21 14:18:36 +0100

    Revert "Onnodige comment verwijderd"
    This reverts commit 59ab6c4879148c51b0e759001517db9e64c62697.

--- src/main/java/corendo/fys/controller/Supervisor_add_brandController.java
+++ src/main/java/corendo/fys/controller/Supervisor_add_brandController.java
@@ -1,6 +1,10 @@
 package corendo.fys.controller;
 
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 import com.jfoenix.controls.JFXTextField;
 import corendo.fys.jdbcDBconnection;
 import java.net.URL;
