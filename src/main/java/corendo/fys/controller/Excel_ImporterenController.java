3835d16ff9d44098cd157dfeeab38143d057b2d2
Ilias Azagagh <ilias.azagagh@gmail.com>
2018-01-02 20:05:09 +0100

    Excel import deel 2
    Het is mogelijk om een excel bestand te importeren zonder foutmeldingen. Alleen de luggage_id en de DateFound worden nu geinsert in de database. De convert to string werkt nog niet goed waardoor luggage_id een iets andere waarde vertoont bij het importeren van bepaalde waardes.

--- src/main/java/corendo/fys/controller/Excel_ImporterenController.java
+++ src/main/java/corendo/fys/controller/Excel_ImporterenController.java
@@ -41,6 +41,8 @@ public class Excel_ImporterenController implements Initializable {
 
     private String filePath;
 
+    XSSFSheet currentSheet;
+
     @FXML
     void on_choose_file(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
@@ -56,35 +58,77 @@ public class Excel_ImporterenController implements Initializable {
 
     @FXML
     void on_import_file(ActionEvent event) throws IOException {
-        // Filepath
-        File file = new File(filePath);
-
-        FileInputStream fis = new FileInputStream(file);
+        // Filepath of the selected file
+        FileInputStream file = new FileInputStream(new File(filePath));
 
         // Specified which file to open
-        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
 
-        // Specified which sheet to open
-        XSSFSheet sheet1 = workbook.getSheetAt(0);
+        int sheetCount = workbook.getNumberOfSheets();
 
-        // Checks what the last filled row is in the file
-        int rowCount = sheet1.getLastRowNum();
+        int k = 0;
 
-        
+        for (int j = 0; j < sheetCount; j++) {
 
-        // Starts at row 5 and prints every first value from each row from the first column
-        for (int i = 4; i < rowCount; i++) {
-            try {
-                String query = "insert INTO luggage_import (luggage_id) VALUES(?)";
-                
-                stmt = conn.prepareStatement(query);
-                stmt.setString(1, sheet1.getRow(i).getCell(0).getStringCellValue().toString());
-                System.out.println(sheet1.getRow(i).getCell(0).getStringCellValue().toString());
-                
-                stmt.execute();
-                stmt.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(Excel_ImporterenController.class.getName()).log(Level.SEVERE, null, ex);
+            currentSheet = workbook.getSheetAt(j);
+            // Checks what the last filled row is in the file
+            int rowCount = currentSheet.getLastRowNum();
+            System.out.println(rowCount);
+
+            // Starts at row 5 and prints every first value from each row from the first column
+            for (int i = 4; i < rowCount; i++) {
+                String luggageId = currentSheet.getRow(i).getCell(0).toString();
+                String dateFound = currentSheet.getRow(i).getCell(1).toString();
+//                String timeFound = currentSheet.getRow(i).getCell(2).toString();
+//                String luggageType = currentSheet.getRow(i).getCell(3).toString();
+//                String brand = currentSheet.getRow(i).getCell(4).toString();
+//                String arrivedFlight = currentSheet.getRow(i).getCell(5).toString();
+//                String luggageTag = currentSheet.getRow(i).getCell(6).toString();
+//                String locationFound = currentSheet.getRow(i).getCell(7).toString();
+//                String mainColor = currentSheet.getRow(i).getCell(8).toString();
+//                String secondColor = currentSheet.getRow(i).getCell(9).toString();
+//                String size = currentSheet.getRow(i).getCell(10).toString();
+//                String weight = currentSheet.getRow(i).getCell(11).toString();
+//                String passengerName = currentSheet.getRow(i).getCell(12).toString().substring(0, currentSheet.getRow(i).getCell(12).toString().indexOf(","));
+//                String city = currentSheet.getRow(i).getCell(12).toString().substring(currentSheet.getRow(i).getCell(12).toString().indexOf(",") + 1, currentSheet.getRow(i).getCell(12).toString().length()).trim();
+//                String otherCharacteristics = currentSheet.getRow(i).getCell(13).toString();
+
+                try {
+                    String insertQuery = "INSERT INTO luggage_import (luggage_id, DateFound) VALUES(?,?)";
+                    String selectQuery = "SELECT luggage_id FROM luggage_import WHERE luggage_id='" + luggageId + "'";
+
+                    stmt = conn.prepareStatement(selectQuery);
+                    rs = stmt.executeQuery();
+
+                    if (rs.next()) {
+                        System.out.println(k++ + " Entry already exists!!!");
+                    } else if(luggageId.isEmpty()){
+                        System.out.println("Value is empty!");
+                    } else {
+                        stmt = conn.prepareStatement(insertQuery);
+                        stmt.setString(1, luggageId);
+                        stmt.setString(2, dateFound);
+//                        stmt.setString(3, currentSheet.getRow(i).getCell(2).toString());
+//                        stmt.setString(4, currentSheet.getRow(i).getCell(3).toString());
+//                        stmt.setString(5, currentSheet.getRow(i).getCell(4).toString());
+//                        stmt.setString(6, currentSheet.getRow(i).getCell(5).toString());
+//                        stmt.setString(7, currentSheet.getRow(i).getCell(6).toString());
+//                        stmt.setString(8, currentSheet.getRow(i).getCell(7).toString());
+//                        stmt.setString(9, currentSheet.getRow(i).getCell(8).toString());
+//                        stmt.setString(10, currentSheet.getRow(i).getCell(9).toString());
+//                        stmt.setString(11, currentSheet.getRow(i).getCell(10).toString());
+//                        stmt.setString(12, currentSheet.getRow(i).getCell(11).toString());
+//                        stmt.setString(13, currentSheet.getRow(i).getCell(12).toString().substring(0, currentSheet.getRow(i).getCell(12).toString().indexOf(",")));
+//                        stmt.setString(14, currentSheet.getRow(i).getCell(12).toString().substring(currentSheet.getRow(i).getCell(12).toString().indexOf(",") + 1, currentSheet.getRow(i).getCell(12).toString().length()).trim());
+//                        stmt.setString(15, currentSheet.getRow(i).getCell(13).toString());
+
+                        stmt.execute();
+                        stmt.close();
+                    }
+
+                } catch (SQLException ex) {
+                    Logger.getLogger(Excel_ImporterenController.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
 
