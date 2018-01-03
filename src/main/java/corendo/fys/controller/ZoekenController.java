454422fa097d66003cb28d19dfd5dafab4034f09
tandohtakyie <32542421+tandohtakyie@users.noreply.github.com>
2018-01-03 12:26:27 +0100

    comments weggehaald

--- src/main/java/corendo/fys/controller/ZoekenController.java
+++ src/main/java/corendo/fys/controller/ZoekenController.java
@@ -1,10 +1,6 @@
 package corendo.fys.controller;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import corendo.fys.jdbcDBconnection;
